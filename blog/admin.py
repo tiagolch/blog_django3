9324003047f16d1398913275e37623c0c26a30b8
Tiago Chaves <tiagodevch@gmail.com>
2020-08-22 13:32:04 -0300

    Acertado campo publish e realizado migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+
+
+
+admin.site.register(Post)
+
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft','Draft'),
@@ -14,7 +15,7 @@ class Post(models.Model):
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
